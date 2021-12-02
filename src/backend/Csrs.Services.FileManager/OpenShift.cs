38d529633ccce0d9794e9ca38157182756f4946f
Phil Bolduc <philbolduc@gmail.com>
2021-12-02 12:58:08 -0800

    Remove BouncyCastle and use native .net 6 functionality

--- src/backend/Csrs.Services.FileManager/OpenShift.cs
+++ src/backend/Csrs.Services.FileManager/OpenShift.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,11 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Math;
-using Org.BouncyCastle.OpenSsl;
-using RedHat.OpenShift.Utils;
 
 // from https://raw.githubusercontent.com/redhat-developer/s2i-dotnetcore-ex/dotnetcore-2.1-https/app/OpenShift.cs
 
@@ -176,85 +170,10 @@ namespace Csrs.Services.FileManager
                         var certificateMountPoint = _options.Value.CertificateMountPoint;
                         var certificateFile = Path.Combine(certificateMountPoint, "tls.crt");
                         var keyFile = Path.Combine(certificateMountPoint, "tls.key");
-                        _certificate = CertificateLoader.LoadCertificateWithKey(certificateFile, keyFile);
+                        _certificate = X509Certificate2.CreateFromPemFile(certificateFile, keyFile);
                     }
 
                 return _certificate;
-            }
-        }
-    }
-}
-
-namespace RedHat.OpenShift.Utils
-{
-    public static class CertificateLoader
-    {
-        public static X509Certificate2 LoadCertificateWithKey(string certificateFile, string keyFile)
-        {
-            var certificate = new X509Certificate2(certificateFile);
-            return certificate.CopyWithPrivateKey(ReadPrivateKeyAsRSA(keyFile));
-        }
-
-        private static RSA ReadPrivateKeyAsRSA(string keyFile)
-        {
-            using (var reader = new StreamReader(new MemoryStream(File.ReadAllBytes(keyFile))))
-            {
-                var obj = new PemReader(reader).ReadObject();
-                if (obj is AsymmetricCipherKeyPair)
-                {
-                    var cipherKey = (AsymmetricCipherKeyPair) obj;
-                    obj = cipherKey.Private;
-                }
-
-                var privKey = (RsaPrivateCrtKeyParameters) obj;
-                return RSA.Create(DotNetUtilities.ToRSAParameters(privKey));
-            }
-        }
-
-        private static class DotNetUtilities
-        {
-            /*
-            +    // This class was derived from:
-            +    // https://github.com/bcgit/bc-csharp/blob/master/crypto/src/security/DotNetUtilities.cs
-            +    // License:
-            +    // The Bouncy Castle License
-            +    // Copyright (c) 2000-2018 The Legion of the Bouncy Castle Inc.
-            +    // (https://www.bouncycastle.org)
-            +    // Permission is hereby granted, free of charge, to any person obtaining a
-            +    // copy of this software and associated documentation files (the "Software"), to deal in the
-            +    // Software without restriction, including without limitation the rights to use, copy, modify, merge,
-            +    // publish, distribute, sub license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-            +    // The above copyright notice and this permission notice shall be included
-            +    // in all copies or substantial portions of the Software.
-            */
-
-            public static RSAParameters ToRSAParameters(RsaPrivateCrtKeyParameters privKey)
-            {
-                var rp = new RSAParameters();
-                rp.Modulus = privKey.Modulus.ToByteArrayUnsigned();
-                rp.Exponent = privKey.PublicExponent.ToByteArrayUnsigned();
-                rp.P = privKey.P.ToByteArrayUnsigned();
-                rp.Q = privKey.Q.ToByteArrayUnsigned();
-                rp.D = ConvertRSAParametersField(privKey.Exponent, rp.Modulus.Length);
-                rp.DP = ConvertRSAParametersField(privKey.DP, rp.P.Length);
-                rp.DQ = ConvertRSAParametersField(privKey.DQ, rp.Q.Length);
-                rp.InverseQ = ConvertRSAParametersField(privKey.QInv, rp.Q.Length);
-                return rp;
-            }
-
-            private static byte[] ConvertRSAParametersField(BigInteger n, int size)
-            {
-                var bs = n.ToByteArrayUnsigned();
-
-                if (bs.Length == size)
-                    return bs;
-
-                if (bs.Length > size)
-                    throw new ArgumentException("Specified size too small", "size");
-
-                var padded = new byte[size];
-                Array.Copy(bs, 0, padded, size - bs.Length, bs.Length);
-                return padded;
             }
         }
     }
