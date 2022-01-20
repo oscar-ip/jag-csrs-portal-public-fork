166be214be97dba23bf7868cf34cb50f11eebd64
Phil Bolduc <phil.bolduc@nttdata.com>
2022-01-19 22:13:30 -0800

    Remove Draft file status, add party enrolled field.

--- src/backend/Csrs.Api/Repositories/CsrsFileRepository.cs
+++ src/backend/Csrs.Api/Repositories/CsrsFileRepository.cs
@@ -17,7 +17,7 @@ namespace Csrs.Api.Repositories
             var entries = await Client
                 .For<SSG_CsrsFile>()
                 .Filter(_ => _.Payor!.PartyId == partyId || _.Recipient!.PartyId == partyId)
-                .Filter(_ => _.StatusCode == SSG_CsrsFile.Active.Id || _.StatusCode == SSG_CsrsFile.Draft.Id)
+                .Filter(_ => _.StatusCode == SSG_CsrsFile.Active.Id)
                 .Select(SSG_CsrsFile.AllProperties)
                 .Expand(_ => _.Payor)
                 .Expand(_ => _.Recipient)
--- src/backend/Csrs.Test/Repositories/ChildInsertOrUpdateFieldMapperTest.cs
+++ src/backend/Csrs.Test/Repositories/ChildInsertOrUpdateFieldMapperTest.cs
@@ -19,10 +19,12 @@ namespace Csrs.Test.Repositories
             Dictionary<string, object>? actual = sut.GetFieldsForInsert(expected);
 
             actual.Should().NotBeNull();
+#pragma warning disable CS8604 // Possible null reference argument.
             actual.Should().Contain(SSG_CsrsChild.Attributes.ssg_firstname, expected.FirstName);
             actual.Should().Contain(SSG_CsrsChild.Attributes.ssg_lastname, expected.LastName);
             actual.Should().Contain(SSG_CsrsChild.Attributes.ssg_middlename, expected.MiddleName);
             actual.Should().Contain(SSG_CsrsChild.Attributes.ssg_dateofbirth, expected.DateOfBirth);
+#pragma warning restore CS8604 // Possible null reference argument.
 
             expected.FirstName = null;
             actual = sut.GetFieldsForInsert(expected);
