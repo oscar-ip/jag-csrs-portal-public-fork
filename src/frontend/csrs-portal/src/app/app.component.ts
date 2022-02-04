6eb46504e101ac93d4d966c80ef500ebefb76669
skiselev <sergey.kiselev@gov.bc.ca>
2022-02-03 21:34:11 -0700

    added snowplow

--- src/frontend/csrs-portal/src/app/app.component.ts
+++ src/frontend/csrs-portal/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouteStateService } from '@core/services/route-state.service';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { LoggerService } from '@core/services/logger.service';
+import { SnowplowService } from '@core/services/snowplow.service';
 
 @Component({
   selector: 'app-root',
@@ -21,12 +22,11 @@ export class AppComponent implements OnInit {
     private metaTagService: Meta,
     private router: Router,
     private logger: LoggerService,
-
+    private snowplow: SnowplowService
   ) {
-
     this.router.events.subscribe((evt) => {
       if (evt instanceof NavigationEnd) {
-
+        this.snowplow.trackPageView();
       }
     });
   }
