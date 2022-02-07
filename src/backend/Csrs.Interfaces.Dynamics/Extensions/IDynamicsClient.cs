f6a59e002ef4d4ab28a80035d9db585d06e39d76
Phil Bolduc <philbolduc@gmail.com>
2022-02-07 09:56:42 -0800

    Add GetEntityURI method to IDynamicsClient

--- src/backend/Csrs.Interfaces.Dynamics/Extensions/IDynamicsClient.cs
+++ src/backend/Csrs.Interfaces.Dynamics/Extensions/IDynamicsClient.cs
@@ -5,4 +5,6 @@ namespace Csrs.Interfaces.Dynamics;
 public partial interface IDynamicsClient
 {
     Task<PicklistOptionSetMetadata> GetPicklistOptionSetMetadataAsync(string entityName, string attributeName, CancellationToken cancellationToken);
+
+    string GetEntityURI(string entityType, string id);
 }
