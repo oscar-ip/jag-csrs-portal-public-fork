e1d7565b10e2394d641412aae2de1e0f0ebf7b2d
TayGov <taylor.clausen@gov.bc.ca>
2022-02-09 07:53:50 -0800

    Fix party check

--- src/backend/Csrs.TntegrationTest/GetPartyTest.cs
+++ src/backend/Csrs.TntegrationTest/GetPartyTest.cs
@@ -15,8 +15,8 @@ namespace Csrs.TntegrationTest
         {
             IDynamicsClient dynamicsClient = _serviceProvider.GetRequiredService<IDynamicsClient>();
 
-            //string filter = $"ssg_bceid_userid ne null";
-            string filter = $"ssg_bceid_guid eq '5beb7384-eb6d-4fd4-8918-c3bb1a5c'";
+            string filter = $"ssg_bceid_userid ne null";
+            //string filter = $"ssg_csrspartyid eq 'fc7702c0-0d89-ec11-b831-00505683fbf4'";
             List<string> expand = new List<string> { "createdby" };
             var actual = await dynamicsClient.Ssgcsrsparties.GetAsync(top: 5, filter: filter, expand: expand, cancellationToken: CancellationToken.None);
             Assert.NotNull(actual);
@@ -27,8 +27,8 @@ namespace Csrs.TntegrationTest
         {
             IDynamicsClient dynamicsClient = _serviceProvider.GetRequiredService<IDynamicsClient>();
             //var actual = await dynamicsClient.GetPartyByBCeIdAsync("f418274f-b10f-ea11-b814-00505683fbf4", cancellationToken: CancellationToken.None);
-            //string actual = await dynamicsClient.GetPartyByBCeIdAsync("5beb7384-eb6d-4fd4-8918-c3bb1a5c", cancellationToken: CancellationToken.None);
-            //Assert.NotNull(actual);
+            var actual = await dynamicsClient.GetPartyByBCeIdAsync("5beb7384-eb6d-4fd4-8918-c3bb1a5c", cancellationToken: CancellationToken.None);
+            Assert.NotNull(actual);
         }
     }
 }
