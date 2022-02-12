6ffb87caa3a380bb149086f9a70d6201797ce7c8
Phil Bolduc <philbolduc@gmail.com>
2022-02-12 14:23:42 -0800

    Fix file summary only returning the human readable file number instead of also the db unqiue id

--- src/backend/Csrs.Api/Models/File.cs
+++ src/backend/Csrs.Api/Models/File.cs
@@ -17,7 +17,15 @@ namespace Csrs.Api.Models
 
     public class FileSummary
     {
+        /// <summary>
+        /// The internal id of the file.
+        /// </summary>
         public string? FileId { get; set; }
+
+        /// <summary>
+        /// The human readable file number.
+        /// </summary>
+        public string? FileNumber { get; set; }
 
         /// <summary>
         /// The file status, Draft or Active.
