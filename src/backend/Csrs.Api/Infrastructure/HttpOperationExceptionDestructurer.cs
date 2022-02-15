f6c818378aaabd37c014313ac89a6548b9fdc3de
Phil Bolduc <philbolduc@gmail.com>
2022-02-15 09:01:00 -0800

    Add exception destructuring, add config for serilog config

---
+++ src/backend/Csrs.Api/Infrastructure/HttpOperationExceptionDestructurer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Rest;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.Destructurers;
+
+namespace Csrs.Api.Infrastructure
+{
+    /// <summary>
+    /// Destructures <see cref="HttpOperationException"/> response properties.
+    /// </summary>
+    public class HttpOperationExceptionDestructurer : ExceptionDestructurer
+    {
+        public override Type[] TargetTypes => new[] { typeof(HttpOperationException) };
+
+        public override void Destructure(
+            Exception exception,
+            IExceptionPropertiesBag propertiesBag,
+            Func<Exception, IReadOnlyDictionary<string, object?>?> destructureException)
+        {
+            base.Destructure(exception, propertiesBag, destructureException);
+
+#pragma warning disable CA1062 // Validate arguments of public methods
+            var targetException = exception as HttpOperationException;
+            if (targetException is not null)
+            {
+                propertiesBag.AddProperty(nameof(HttpOperationException.Response.StatusCode), targetException.Response.StatusCode);
+                propertiesBag.AddProperty(nameof(HttpOperationException.Response.ReasonPhrase), targetException.Response.ReasonPhrase);
+                propertiesBag.AddProperty(nameof(HttpOperationException.Response.Content), targetException.Response.Content);
+            }
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+    }
+}
