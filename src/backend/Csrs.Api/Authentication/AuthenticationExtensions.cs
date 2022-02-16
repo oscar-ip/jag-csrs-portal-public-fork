1b76ef36c0e711fa62ce2f667649a50140b30f9e
Phil Bolduc <PhilBolduc@gmail.com>
2022-02-16 10:32:39 -0800

    Simplify JWT auth configuration based on sample code (#132)

--- src/backend/Csrs.Api/Authentication/AuthenticationExtensions.cs
+++ src/backend/Csrs.Api/Authentication/AuthenticationExtensions.cs
@@ -1,81 +1,18 @@
-﻿using Csrs.Api.Configuration;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Serilog;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class AuthenticationExtensions
 {
-    private const string ConfigurationKey = "Jwt";
-
     public static void AddJwtBearerAuthentication(this WebApplicationBuilder builder)
     {
-        builder.Services.Configure<JwtBearerOptions>(options => builder.Configuration.GetSection(ConfigurationKey).Bind(options));
-        LogConfiguration(builder);
-
-        builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-            options.Events = new JwtBearerEvents
+        // https://github.com/dotnet/aspnetcore/blob/v6.0.2/src/Security/Authentication/JwtBearer/samples/JwtBearerSample/Startup.cs
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
             {
-                OnAuthenticationFailed = c =>
-                {
-                    var logger = Log.Logger;
-                    if (c.Exception != null)
-                    {
-                        logger.Debug(c.Exception, "Authentication Failed Context: {@AuthenticationFailedContext}", c);
-                    }
-                    else
-                    {
-                        logger.Debug("Authentication Failed Context: {@AuthenticationFailedContext}", c);
-                    }
-
-                    c.NoResult();
-
-                    c.Response.StatusCode = 401;
-                    c.Response.ContentType = "text/plain";
-
-                    if (builder.Environment.IsDevelopment() && c.Exception is not null)
-                    {
-                        return c.Response.WriteAsync(c.Exception.ToString());
-                    }
-
-                    return c.Response.WriteAsync("An error occured processing your authentication.");
-                }
-            };
-        });
-    }
-
-    private static void LogConfiguration(WebApplicationBuilder builder)
-    {
-        var options = new JwtBearerOptions();
-        Bind(builder, options);
-        Serilog.ILogger logger = GetLogger();
-        logger.Information("JWT Authentication settings {@JwtBearerOptions}", new { options.Audience, options.Authority, options.MetadataAddress });
-    }
-
-    /// <summary>
-    /// Binds the Jwt configuration to the options
-    /// </summary>
-    /// <param name="builder"></param>
-    /// <param name="options"></param>
-    private static void Bind(WebApplicationBuilder builder, JwtBearerOptions options) => builder.Configuration.Bind(ConfigurationKey, options);
-
-    /// <summary>
-    /// Gets a logger for application setup.
-    /// </summary>
-    private static Serilog.ILogger GetLogger()
-    {
-        var logger = new LoggerConfiguration()
-            .WriteTo.Console()            
-            .WriteTo.Debug()            
-            .MinimumLevel.Debug()
-            .CreateLogger();
-
-        return logger;
+                options.Authority = builder.Configuration["Jwt:Authority"];
+                options.Audience = builder.Configuration["Jwt:Audience"];
+            });
     }
 }
