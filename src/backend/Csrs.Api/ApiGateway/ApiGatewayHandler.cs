ea74a389e1aefef5b3692b7bae1bf00097e2062d
skiselev <sergey.kiselev@gov.bc.ca>
2022-02-17 21:05:46 -0700

    restored previous code

--- src/backend/Csrs.Api/ApiGateway/ApiGatewayHandler.cs
+++ src/backend/Csrs.Api/ApiGateway/ApiGatewayHandler.cs
@@ -13,11 +13,13 @@ namespace Csrs.Api.ApiGateway
     {
 
         public readonly ApiGatewayOptions _apiGatewayOptions;
+        private readonly ILogger<ApiGatewayHandler> _logger;
 
         public ApiGatewayHandler(
-            IOptions<ApiGatewayOptions> apiGatewayOptions)
+            IOptions<ApiGatewayOptions> apiGatewayOptions, ILogger<ApiGatewayHandler> logger)
         {
             _apiGatewayOptions = apiGatewayOptions.Value;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -37,6 +39,7 @@ namespace Csrs.Api.ApiGateway
                                     Encoding.UTF8,
                                     "application/json");//CONTENT-TYPE header
 
+                _logger.LogInformation("The _apiGatewayOptions.BasePath is ", path);
                 request.RequestUri = path;
             }
 
