2224842d41ddd919ca70a19d2fbe76c466f1585d
Phil Bolduc <PhilBolduc@gmail.com>
2022-02-28 17:27:54 -0800

    Add new OpenShift integration code based on working project (#206)
    * Remove auth requirement, check some args for null, add logging
    
    * Replace OpenShift Integration

---
+++ src/backend/Csrs.Services.FileManager/OpenShiftIntegration/OpenShiftCertificateLoader.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Csrs.Services.FileManager.OpenShiftIntegration
+{
+    /// <summary>
+    /// Reads the certificate from the certificate mount point.
+    /// </summary>
+    internal class OpenShiftCertificateLoader
+    {
+        private readonly IOptions<OpenShiftIntegrationOptions> _options;
+
+        public OpenShiftCertificateLoader(IOptions<OpenShiftIntegrationOptions> options)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public X509Certificate2 GetCertificateWithKey()
+        {
+            var certificateMountPoint = _options.Value.CertificateMountPoint;
+            var certificateFile = Path.Combine(certificateMountPoint, "tls.crt");
+            var keyFile = Path.Combine(certificateMountPoint, "tls.key");
+            var certificate = X509Certificate2.CreateFromPemFile(certificateFile, keyFile);
+
+            return certificate;
+        }
+
+        public X509Certificate2Collection GetCertificateChain()
+        {
+            var certificateMountPoint = _options.Value.CertificateMountPoint;
+            var fullChain = new X509Certificate2Collection();
+            fullChain.ImportFromPemFile(Path.Combine(certificateMountPoint, "tls.crt"));
+            return fullChain;
+        }
+    }
+
+}
---
+++ src/backend/Csrs.Services.FileManager/OpenShiftIntegration/OpenShiftIntegrationOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Csrs.Services.FileManager.OpenShiftIntegration
+{
+    /// <summary>
+    /// Contains the location where the TLS certificate should be stored.
+    /// </summary>
+    public class OpenShiftIntegrationOptions
+    {
+        public string CertificateMountPoint { get; set; }
+
+        internal bool UseHttps => !string.IsNullOrEmpty(CertificateMountPoint);
+    }
+}
