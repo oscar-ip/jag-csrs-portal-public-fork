cb3a9cec24422c17e9e456dafd8ad82a769a0b11
skiselev <sergey.kiselev@gov.bc.ca>
2022-03-03 13:49:02 -0700

    CSRS394

--- src/frontend/csrs-portal/src/app/shared/components/header/header.component.ts
+++ src/frontend/csrs-portal/src/app/shared/components/header/header.component.ts
@@ -5,11 +5,13 @@ import {
   EventEmitter,
   Input,
   OnInit,
+  Inject,
 } from '@angular/core';
 import { LoggerService } from '@core/services/logger.service';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfigService } from 'app/services/app-config.service';
-
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -20,24 +22,64 @@ export class HeaderComponent implements OnInit {
   public fullName: string;
   @Output() public toggle: EventEmitter<void>;
 
-  public environment: string;
-  public version: string;
+  public icon: string = '';
+  public iconText:string = '';
 
-  constructor(
-    protected logger: LoggerService,
-    private appConfigService: AppConfigService
-  ) {
-    this.toggle = new EventEmitter<void>();
- 
-    this.environment = this.appConfigService.environment;
-    this.version = this.appConfigService.version;
+  constructor(public oidc : OidcSecurityService,
+              protected logger: LoggerService,
+              private appConfigService: AppConfigService,
+              @Inject(Router) private router) {
+
+          this.toggle = new EventEmitter<void>();
   }
 
   public async ngOnInit() {
+
+    this.icon = 'login';
+    this.iconText = 'BCeID Login';
+    this.oidc.checkAuth().subscribe(({ isAuthenticated }) => {
+      this.logger.info('isAuthenticated: ', isAuthenticated);
+      if (isAuthenticated === true)
+      {
+        this.router.navigate(['/welcomeuser']);
+      }
+      else
+      {
+        this.router.navigate(['/']);
+      }
+
+    });
+
   }
 
   public toggleSidenav(): void {
     this.toggle.emit();
+  }
+
+  public changeIcon(): void{
+    this.logger.warn(`inside: icon: ${this.icon} text: ${this.iconText}`);
+
+    if (this.icon === 'login')
+    {
+      this.icon = 'logout';
+      this.iconText = 'BCeID Logout';
+      this.login();
+    }
+    else
+    {
+      this.icon = 'login';
+      this.iconText = 'BCeID Login';
+      this.logout();
+    }
+    this.logger.warn(`return: icon: ${this.icon} text: ${this.iconText}`);
+  }
+
+  login() {
+    this.oidc.authorize();
+  }
+
+  logout() {
+    this.oidc.logoff();
   }
 
 }
