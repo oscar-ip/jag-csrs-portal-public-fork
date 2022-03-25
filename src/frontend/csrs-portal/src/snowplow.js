8fae03f57e95792861e2943ef987cb57f7252991
skiselev <sergey.kiselev@gov.bc.ca>
2022-03-24 22:06:19 -0600

    removed snowplow custom code, fixed issue with bceIdRegisterLink

--- src/frontend/csrs-portal/src/snowplow.js
+++ src/frontend/csrs-portal/src/snowplow.js
@@ -1,20 +1,19 @@
+
 // <!-- Snowplow starts plowing - Standalone vE.2.14.0 -->
+//import { readFileSync } from 'fs';
 ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
   p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
   };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
   n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://www2.gov.bc.ca/StaticWebResources/static/sp/sp-2-14-0.js","snowplow"));
 
-  /*
-  var collector = '';
+/*
   let configFile = new XMLHttpRequest();
   configFile.open('GET', './assets/config/app.config.json');
   configFile.responseType = 'json';
   configFile.send();
   configFile.onload = function() {
-    collector = configFile.response.collector;
-  };
-  console.log('collector: ', collector);
-  */
+    const collector = configFile.response.collector;
+    console.log('collector: ', collector);*/
 
   const collector = "spm.apps.gov.bc.ca";
   window.snowplow('newTracker','rt',collector, {
@@ -32,3 +31,5 @@
   window.snowplow('enableLinkClickTracking');
   //window.snowplow('trackPageView');
  // <!-- Snowplow stops plowing -->
+
+/*}*/
