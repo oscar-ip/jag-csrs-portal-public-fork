cca0a945bdf50e5585d675d7de74562448ebfe16
KKAYFISH <kyle.kayfish@gmail.com>
2024-03-27 13:54:17 -0700

    CSRS-488:  Added underline and Terms of Use to Footer

--- src/frontend/csrs-portal/src/app/shared/components/footer/footer.component.ts
+++ src/frontend/csrs-portal/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalDialogHtmlComponent } from '../../../components/modal-dialog-htmlcontent/modal-dialog-htmlcontent.component';
+
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog,) { }
 
   ngOnInit(): void {
   }
+  showTermsOfUse(): void {
+    const dialogRef = this.dialog.open(ModalDialogHtmlComponent, {
+      width: '750px',
+      data: '',
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      //this.logger.info(`Dialog result: ${result}`);
+    });
+  }
 }
